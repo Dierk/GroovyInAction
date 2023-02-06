66c70166789bda20af7b757c5dc7b242f0868d36
Dierk Koenig <dierk.koenig@canoo.com>
2023-02-06 16:35:23 +0100

    adapt the numbers of properties, methods, and metaMethods on String.metaClass for Groovy 4 and Java 19 and make it more stable against future changes

--- listings/chap08/Listing_08_07_MetaClass_jdk8_plus.groovy
+++ listings/chap08/Listing_08_07_MetaClass_jdk8_plus.groovy
@@ -2,8 +2,8 @@ MetaClass mc = String.metaClass
 final Object[] NO_ARGS = []
 assert   1  == mc.respondsTo("toString", NO_ARGS).size()
 assert   5  == mc.properties.size()
-assert  93  == mc.methods.size()
-assert 220  == mc.metaMethods.size()
+assert  90  <  mc.methods.size()
+assert 200  <  mc.metaMethods.size()
 assert ""   == mc.invokeMethod("","toString", NO_ARGS)
 assert null == mc.invokeStaticMethod(String, "println", NO_ARGS)
 assert ""   == mc.invokeConstructor(NO_ARGS)
