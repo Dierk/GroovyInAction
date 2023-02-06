965869391fe87296c0bf13c30829e42139f407db
Dierk Koenig <dierk.koenig@canoo.com>
2023-02-06 19:20:51 +0100

    WARN: update String.dump() usage

--- listings/chap12/Listing_12_01_info_jdk8_plus.groovy
+++ listings/chap12/Listing_12_01_info_jdk8_plus.groovy
@@ -2,8 +2,14 @@ def newline = "\n"
 
 assert newline.toString() == "\n"
 
+/* due to module security, this info is no longer available
+   updated in 4.0.8
 assert newline.dump() ==
     '''<java.lang.String@a value=
  hash=10>'''
+ */
+
+println "WARN: update String.dump() usage!"
+// TODO: update String.dump() usage
 
 assert newline.inspect() == /'\n'/
