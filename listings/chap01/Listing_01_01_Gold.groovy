c9728b48f505d387bc8dfbf53e89bda09f41edc6
Dierk Koenig <dierk.koenig@canoo.com>
2023-02-06 11:16:35 +0100

    size is a function

--- listings/chap01/Listing_01_01_Gold.groovy
+++ listings/chap01/Listing_01_01_Gold.groovy
@@ -6,7 +6,7 @@ while ( ! isGolden( gold[-1] ) ) {          //#C
     gold.add( fibo[-1] / fibo[-2] )         //#E
 }
 
-println "found golden ratio with fibo(${ fibo.size-1 }) as"
+println "found golden ratio with fibo(${ fibo.size()-1 }) as"
 println fibo[-1] + " / " + fibo[-2] + " = " + gold[-1]
 println "_" * 10 +  "|"  + "_" * (10 * gold[-1])
 
