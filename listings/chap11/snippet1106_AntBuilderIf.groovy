da4f95dc87c57c8e047949d038235a56186a50c4
Dierk Koenig <dierk.koenig@canoo.com>
2023-02-06 19:06:26 +0100

    ant package prefix

--- listings/chap11/snippet1106_AntBuilderIf.groovy
+++ listings/chap11/snippet1106_AntBuilderIf.groovy
@@ -1,4 +1,4 @@
-ant = new AntBuilder()
+ant = new groovy.ant.AntBuilder()
 if ( ! System.properties.'java.version'.contains('1.7')) {
     ant.fail 'This build script requires JDK 1.7.x but was ' +
         System.properties.'java.version'
