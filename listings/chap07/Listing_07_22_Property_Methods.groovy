95e35a55555191b86e2e0dc58489cbdb74b5b064
Dierk Koenig <dierk.koenig@canoo.com>
2023-02-06 15:54:09 +0100

    all "field"-like declarations in a class become properties, regardless of visibility modifier

--- listings/chap07/Listing_07_22_Property_Methods.groovy
+++ listings/chap07/Listing_07_22_Property_Methods.groovy
@@ -12,8 +12,11 @@ obj.properties.each { property ->
     store += property.value
 }
 assert store.contains('someProperty')
-assert store.contains('someField')        == false 
-assert store.contains('somePrivateField') == false
+assert store.contains('someField')
+assert store.contains('somePrivateField')
 assert store.contains('class')
 
-assert obj.properties.size() == 2
+assert obj.properties.size() == 4
+
+// new: all the above are fields
+// regardless of visibility modifier
